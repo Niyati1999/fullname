5c62527a58bcea526b4ebe7aaf468a22dae7ca37
niyati salot <170120107148@git.org.in>
2020-12-23 10:05:45 +0530

    Add project files.

---
+++ fullname/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace fullname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Name: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Middle Name: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            string c = Console.ReadLine();
+            Console.WriteLine("Full Name = " +a + " " + b + " " + c);
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Value of i = " + i);
+        }
+    }
+}
